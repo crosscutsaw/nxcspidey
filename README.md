7d77ba75db248821cd3a59efb6425abf6008f86b
crosscutsaw <73831924+crosscutsaw@users.noreply.github.com>
2022-09-11 20:10:56 +0300

    v1.1

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
+## about cmespidey v1.1
 cmespidey is a bash script that uses crackmapexec's share spider feature, but in bulk.
-
-how to use?
-
--> put your hosts to p445.txt file
-
+## how to use
+-> put your port 445 ip's to p445.txt file
 -> add or remove desired file extensions in script (share_extensions line)
-
+-> adjust threads if you want (default: -t 80)
 -> execute smbspidey.sh
-
 -> profit
-
-example output from my ad lab:
+## extensions explanation
+-> .vhd >>> fixed suffix. it can be emre.vhd, important.vhd, deadasscompanydc01.vhd.. etc
+-> config. >>> fixed prefix. it can be config.cfg, config.pkcs, config.deadass.. etc
+-> credentials, dbpass, cpassword >>> it can be both filename or matching pattern in file. intended use is matching pattern.
+## example output
 ![](https://raw.githubusercontent.com/crosscutsaw/cmespidey/main/output.PNG)
-not tested on real active directory environment scenario
+*still not tested in customer active directory environment*
--- cmespidey.sh
+++ cmespidey.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# cmespidey
-# 
-# contact options
-# mail: https://vvmlist.github.io/contact.html
-# twitter: https://twitter.com/tasiyanci
-# linkedin: https://linkedin.com/in/aslanemreaslan
-
 domain_user="domainuser"
 domain_pass='domainpass'
-share_extensions="vhd vbk vmdk iso cfg config"
+share_extensions=".vhd .vbk .vmdk .cfg .config .conf credentials db. config. group groups pwd pass password dbpass dbpwd db_password db_pass sifre şifre cpassword"
 
 crackmapexec smb p445.txt -u $domain_user -p $domain_pass --shares > cmespidey_temp.txt
 
 for i in $(egrep -e "(WRITE|READ)" cmespidey_temp.txt | awk '{print $2}' | uniq); do
 	for j in $(egrep -e "(WRITE|READ)" cmespidey_temp.txt | awk '{print $5}' | cut -c 10- | uniq); do
-		crackmapexec smb $i -u $domain_user -p $domain_pass --spider $j --pattern $share_extensions | grep size >> cmespidey.txt
+		crackmapexec -t 80 smb $i -u $domain_user -p $domain_pass --spider $j --pattern $share_extensions --content | grep size | sed 's/\(pattern\)/\o033[31m\1\o033[39m/' >> cmespidey.txt
 	done	
 done
 rm -rf cmespidey_temp.txt
 echo "####################"; echo "process is completed"; echo "####################"; echo ""
 cat cmespidey.txt | sort | uniq
+
+# cmespidey 1.1
+# 
+# contact options
+# mail: https://vvmlist.github.io/contact.html
+# twitter: https://twitter.com/tasiyanci
+# linkedin: https://linkedin.com/in/aslanemreaslan
